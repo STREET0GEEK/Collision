2d453cd3433c95899e34c71a7915d2794392b8d9
Nicolas <Asmann.nicolas@yahoo.com>
2019-04-06 13:10:48 +0300

    Q update

---
+++ pI CALCULATOR/Assets/CounterBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterBlock : MonoBehaviour {
+    public Rigidbody2D Countingrb;
+
+    public float vel = 0;
+    public float mass1 = 1;
+    public float mass2 = 1;
+    public float vel2 = -1;
+
+    void Start () {
+        Countingrb = Countingrb.GetComponent<Rigidbody2D>();
+    }
+	
+	void Update () {
+        //Debug.Log(Countingrb.velocity);
+	}
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        vel = ((mass1 - mass2) / (mass1 + mass2)) * vel + ((2*mass2)/mass1 + mass2) * vel2;
+        Countingrb.velocity = new Vector3(vel, 0, 0);
+        Debug.Log(tag);
+        //print("hellothere");
+    }
+}
---
+++ pI CALCULATOR/Assets/ForcingBlock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcingBlock : MonoBehaviour {
+    public Rigidbody2D Forcingrb;
+
+
+    public float vel = -1;
+    public float mass1 = 1;
+    public float mass2 = 1;
+    public float vel2 = 0;
+
+
+    void Start () {
+        Forcingrb =Forcingrb.GetComponent<Rigidbody2D>();
+        Forcingrb.velocity = new Vector3(vel, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        //Debug.Log(hit);
+        vel = ((2 * mass1) / mass1 + mass2) * vel + ((mass2 - mass1)/(mass1+ mass2)) * vel2;
+    }
+}
