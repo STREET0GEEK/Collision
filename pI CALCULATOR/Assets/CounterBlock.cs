e5e3dece2873c96f338211ebcf144f15cf47d03b
Nicolas <Asmann.nicolas@yahoo.com>
2019-04-07 09:18:20 +0300

    it just needs to be optimized
    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet

--- pI CALCULATOR/Assets/CounterBlock.cs
+++ pI CALCULATOR/Assets/CounterBlock.cs
@@ -6,32 +6,49 @@ public class CounterBlock : MonoBehaviour {
     public Rigidbody2D Countingrb;
     public ForcingBlock forcingscript;
 
+    public float numberofhits;
+
+
     public float vel = 0;
     public float mass1 = 1;
     public float mass2 = 1;
     public float vel2 = -1;
 
+    public float repeats = 50;
+
+
     void Start () {
+        repeats = forcingscript.repeats;
         Countingrb = Countingrb.GetComponent<Rigidbody2D>();
+        Countingrb.mass = mass1;
     }
 	
 	void Update () {
-        vel2 = forcingscript.vel2;
+            vel2 = forcingscript.vel2;
+            mass2 = forcingscript.mass2;
 	}
+
     void OnCollisionEnter2D(Collision2D hit)
     {
 
-        mass2 = hit.collider.attachedRigidbody.mass;
-        if(hit.collider.tag != "Wall")
+            mass2 = hit.collider.attachedRigidbody.mass;
+
+        if (hit.collider.tag != "Wall")
         {
             vel = ((mass1 - mass2) / (mass1 + mass2)) * vel + ((2 * mass2) / (mass1 + mass2)) * vel2;
             Countingrb.velocity = new Vector3(vel, 0, 0);
+            numberofhits++;
+
         }
         else
         {
+
             //Vector3 velo = new Vector3(vel * -1, 0, 0);
             vel *= -1;
             Countingrb.velocity = new Vector2(vel, 0);
+            numberofhits++;
         }
+        
     }
+
 }
--- pI CALCULATOR/Assets/ForcingBlock.cs
+++ pI CALCULATOR/Assets/ForcingBlock.cs
@@ -5,41 +5,40 @@ using UnityEngine;
 public class ForcingBlock : MonoBehaviour {
     public Rigidbody2D Forcingrb;
     public CounterBlock counterscript;
+    public float repeats = 10;
 
     public float vel = 0;
     public float mass1 = 1;
-    public float mass2 = 1;
+    public float mass2 = 100000;
     public float vel2 = -1;
+
 
 
     public float debugfloat;
 
 
     void Start () {
-
-        debugfloat = (((2 * 1) / (1 + 1)) * 0) + (((1 - 1) / (1 + 1)) * -1);
-        Debug.Log(debugfloat);
-        Forcingrb =Forcingrb.GetComponent<Rigidbody2D>();
+        //vel2 = vel2 / repeats;
+        //debugfloat = (((2 * 1) / (1 + 1)) * 0) + (((1 - 1) / (1 + 1)) * -1);
+        //Debug.Log(debugfloat);
+        Forcingrb = Forcingrb.GetComponent<Rigidbody2D>();
         Forcingrb.velocity = new Vector3(vel2, 0, 0);
+        Forcingrb.mass = mass2;
+
     }
 
     void Update () {
-
-        vel = counterscript.vel;
+            vel = counterscript.vel;
+            mass1 = counterscript.mass1;
 
     }
     void OnCollisionEnter2D(Collision2D hit)
     {
-
-        //Debug.Log(hit);
-        mass1 = hit.collider.attachedRigidbody.mass;
-        //vel = hit.collider.attachedRigidbody.velocity.magnitude;
-
-        Debug.Log(new Vector4(mass1, mass2, vel, vel2));
-
-        vel2 = (((2*mass1)/(mass1+mass2))*vel) + (((mass2-mass1)/(mass1+mass2))*vel2);
-
-        Forcingrb.velocity = new Vector3(vel2, 0, 0);
-        // Forcingrb.velocity = new Vector3(vel, 0, 0);
+        for (int i = 10; i <= 10; i++)
+        {
+            mass1 = hit.collider.attachedRigidbody.mass;
+            vel2 = (((2 * mass1) / (mass1 + mass2)) * vel) + (((mass2 - mass1) / (mass1 + mass2)) * vel2);
+            Forcingrb.velocity = new Vector3(vel2, 0, 0);
+        }
     }
 }
