0c39baffda4de3108249ade5fabd3410497ab17e
Nicolas <Asmann.nicolas@yahoo.com>
2019-04-07 19:49:35 +0300

    lit
    littttttttttt

---
+++ pI CALCULATOR/Library/Collab/Base/Assets/ForcingBlock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcingBlock : MonoBehaviour {
+    public Rigidbody2D Forcingrb;
+	// Use this for initialization
+	void Start () {
+        Forcingrb =Forcingrb.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Forcingrb.velocity = new Vector3(-3, 0, 0);
+	}
+}
---
+++ pI CALCULATOR/Library/Collab/Original/Assets/ForcingBlock.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcingBlock : MonoBehaviour {
+    public Rigidbody2D Forcingrb;
+    public CounterBlock counterscript;
+    public float repeats = 10;
+
+    public float vel = 0;
+    public float mass1 = 1;
+    public float mass2 = 100000;
+    public float vel2 = -1;
+
+
+
+    public float debugfloat;
+
+
+    void Start () {
+        //vel2 = vel2 / repeats;
+        //debugfloat = (((2 * 1) / (1 + 1)) * 0) + (((1 - 1) / (1 + 1)) * -1);
+        //Debug.Log(debugfloat);
+        Forcingrb = Forcingrb.GetComponent<Rigidbody2D>();
+        Forcingrb.velocity = new Vector3(vel2, 0, 0);
+        Forcingrb.mass = mass2;
+
+    }
+
+    void Update () {
+            vel = counterscript.vel;
+            mass1 = counterscript.mass1;
+
+    }
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        for (int i = 10; i <= 10; i++)
+        {
+            mass1 = hit.collider.attachedRigidbody.mass;
+            vel2 = (((2 * mass1) / (mass1 + mass2)) * vel) + (((mass2 - mass1) / (mass1 + mass2)) * vel2);
+            Forcingrb.velocity = new Vector3(vel2, 0, 0);
+        }
+    }
+}
